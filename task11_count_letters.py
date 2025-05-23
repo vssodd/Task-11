81be19fd14d3e3cb7250a349d711089cab74e50a
Vladyslav Synytskyi <vladyslavsynytskyi1019@gmail.com>
2025-05-23 22:37:23 +0300

    Create task11_count_letters.py
    This function takes a string text and counts how many times a specified digit and a specified letter appear in it. It then prints the counts of both the digit and the letter in the given text.

---
+++ task11_count_letters.py
@@ -0,0 +1,6 @@
+def count_letters(text, digit, letter):
+    digit_count = text.count(digit)
+    letter_count = text.count(letter)
+  
+    print('Number of digits', digit, ':', digit_count)
+    print('Number of letters', letter, ':', letter_count)
