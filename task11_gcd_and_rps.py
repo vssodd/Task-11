5cec0d61c37045c759f0ef6c5402e25206715c5a
Vladyslav Synytskyi <vladyslavsynytskyi1019@gmail.com>
2025-05-23 22:40:42 +0300

    Create task11_gcd_and_rps.py
    The program first takes two integers a and b from the user and prints their greatest common divisor using the gcd function (assumed defined earlier).
    
    Then, it defines a simple rock_paper_scissors() game where the computer randomly selects an option, and the user inputs theirs. The game then determines the winner or if it is a draw.

---
+++ task11_gcd_and_rps.py
@@ -0,0 +1,18 @@
+a = int(input('Enter number a: '))
+b = int(input('Enter number b: '))
+print(gcd(a, b))
+
+import random
+
+def rock_paper_scissors():
+    choices = ['Rock', 'Scissors', 'Paper']
+    computer_choice = random.choice(choices)
+    user_choice = input('Enter your choice (Rock, Scissors, Paper): ')
+    if user_choice == computer_choice:
+        print('Draw!')
+    elif (user_choice == 'Rock' and computer_choice == 'Scissors') or \
+         (user_choice == 'Scissors' and computer_choice == 'Paper') or \
+         (user_choice == 'Paper' and computer_choice == 'Rock'):
+        print('You win!')
+    else:
+        print('You lose!')
